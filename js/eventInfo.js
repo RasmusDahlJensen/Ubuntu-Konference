494872bca1a0467cdf77f86eacaf4d55c5b1a2eb
Rasmus Dahl Jensen <rasmusdj96@gmail.com>
2022-09-01 14:31:26 +0200

    Small changes

---
+++ js/eventInfo.js
@@ -0,0 +1,27 @@
+//Object
+
+
+//speaker links
+//IOT
+// const olav = document.getElementById("olavEriksen")
+const olav = document.getElementById("olavEriksen")
+const mamouhd = document.getElementById("mamouhdHassan")
+const louise = document.getElementById("mamouhdHassan")
+const joe = document.getElementById("joePeterson")
+const alice = document.getElementById("aliceJennings")
+
+//Desktop
+const sara = document.getElementById("saraSouidan")
+const lis = document.getElementById("lisNielsen")
+const line = document.getElementById("lineDalsgaard")
+const dan = document.getElementById("danJantzen")
+const chris = document.getElementById("chrisSmith")
+
+
+//event container
+// const container = document.getElementById("eventContainer")
+
+const nav = document.getElementsByTagName("nav")
+
+
+
